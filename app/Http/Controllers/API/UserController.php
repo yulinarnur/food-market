fa835519a84cec3c26f0376b896b7ad929cccc3a
YulinarNR <yulinarnurrahma@gmail.com>
2023-09-09 14:34:26 +0700

    API untuk data user, update profil, update foto

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -10,6 +10,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use App\Actions\Fortify\PasswordValidationRules;
+use illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -95,4 +96,45 @@ class UserController extends Controller
         return ResponseFormatter::success($token, 'Token Revoked');
     }
 
+    public function fetch(Request $request){ 
+        return ResponseFormatter::success(
+            $request->user(),'Data profile user berhasil diambil');
+    }
+
+    public function updateProfile(Request $request){
+        $data = $request->all();
+
+        $user = Auth::user();
+        $user->update($data);
+
+        return ResponseFormatter::success($user, 'Profile Updated');
+    }
+
+    public function updatePhoto(Request $request){
+        $validator = Validator::make($request->all(),[
+            'file' => 'required|image|max:2048'
+        ]);
+
+        if($validator->fails())
+        {
+            return ResponseFormatter::error(
+                ['error' => $validator->errors()],
+                'Update photo fails',
+                401
+            );
+        }
+
+        if ($request->file('file'))
+        {
+            $file = $request->file->store('assets/user','public');
+
+            // Simpan foto ke database (url-nya)
+            $user = Auth::user();
+            $user->profile_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File successfully uploaded');
+        }
+    }
+    
 }
