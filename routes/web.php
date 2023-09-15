a8e7ce6cb180cf6f68863d61506fd4c1dd992bdb
YulinarNR <yulinarnurrahma@gmail.com>
2023-09-15 21:36:35 +0700

    Halaman dashboar

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\API\MidtransController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +15,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+// Home page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Dashboard
+Route::prefix('dashboard')
+    ->middleware(['auth:sanctum','admin'])
+    ->group(function() {
+        Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
+    });
 
 // Midtrans
 Route::get('midtrans/success', [MidtransController::class,'success']);
