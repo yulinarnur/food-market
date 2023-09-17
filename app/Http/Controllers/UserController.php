8ce6ecc62bcc6840878a3fd45aeea8a575f20eb4
YulinarNR <yulinarnurrahma@gmail.com>
2023-09-17 21:43:39 +0700

    CRUD User delete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -91,8 +91,10 @@ class UserController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        //
+        $user->delete();
+
+        return redirect()->route('users.index');
     }
 }
--- resources/views/users/navigation-dropdown.blade.php
+++ resources/views/users/navigation-dropdown.blade.php
@@ -17,8 +17,8 @@
                     </x-jet-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="/dashboard/users" :active="request()->routeIs('dashboard/users')">
-                        {{ __('Users') }}
+                    <x-jet-nav-link href="{{ route('dashboard') }}" :active="request()->routeIs('dashboard')">
+                        {{ __('Dashboard') }}
                     </x-jet-nav-link>
                 </div>
             </div>
