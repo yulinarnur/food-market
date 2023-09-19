38377e8ac7e594e6a1ae820a57a6fbc1edb86eea
YulinarNR <yulinarnurrahma@gmail.com>
2023-09-19 10:18:58 +0700

    CRUD Transaksi controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\API\MidtransController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\FoodController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
@@ -29,6 +30,8 @@ Route::prefix('dashboard')
         Route::get('/',[DashboardController::class, 'index'])->name('dashboard');
         Route::resource('users', UserController::class);
         Route::resource('food', FoodController::class);
+
+        Route::resource('transaction', TransactionController::class);
     });
 
 // Midtrans
